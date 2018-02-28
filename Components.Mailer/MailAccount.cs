160f19696625050bc1e6026f44859fd6288b692b
enisn <iletisim@enisnecipoglu.com>
2018-02-28 12:48:47 +0300

    Add project files.

---
+++ Components.Mailer/MailAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.Mailer
+{
+    public class MailAccount
+    {
+        /// <summary>
+        /// For Example "smtp.google.com" or "smtp.yandex.com"
+        /// </summary>
+        public string StmpAddress { get; set; } = "smtp.google.com";
+        /// <summary>
+        /// PortNumber of your mailserver
+        /// Gmail : 587
+        /// Yandex: 465
+        /// or your custom port
+        /// </summary>
+        public int PortNumber { get; set; }
+
+        /// <summary>
+        /// Use SSL in Connection
+        /// </summary>
+        public bool EnableSSL { get; set; }
+        /// <summary>
+        /// EmailAddress
+        /// </summary>
+        public string EmailAdress { get; set; }
+        /// <summary>
+        /// Email Password
+        /// </summary>
+        public string EmailPassword { get; set; }
+        public MailAccount()
+        {
+
+        }
+        public MailAccount(string stmp, int port, string email, string password)
+        {
+            StmpAddress = stmp; PortNumber = port; EmailAdress = email; EmailPassword = password;
+        }
+        public MailAccount(string stmp, int port, string email, string password,bool ssl):this(stmp,port,email,password)
+        {
+            EnableSSL = ssl;
+        }
+    }
+}
---
+++ Components.Mailer/Sender.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.Mailer
+{
+    public class Sender
+    {
+        internal const string smtpAddress = "smtp.yandex.com";
+        internal const int portNumber = 465;
+        internal const bool enableSSL = true;
+
+        internal const string emailFrom = "noreply@conetwork.info";
+        internal const string password = "PASSWORD HERE";
+
+        public static bool Send(string targetMail, string title, string content)
+        {
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                {
+
+                    mail.From = new MailAddress(emailFrom);
+                    mail.To.Add(targetMail);
+                    mail.Subject = title;
+                    mail.Body = content;
+                    mail.IsBodyHtml = true;
+
+                    using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                    {
+                        smtp.Credentials = new NetworkCredential(emailFrom, password);
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                // throw ex;
+                return false;
+            }
+        }
+    }
+}
